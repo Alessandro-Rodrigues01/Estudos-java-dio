0df08d2f30e8282fe0bc2581fdfbbe6cc9795a2a
Alessandro <alessandro.rodrigues089@gmail.com>
2024-05-21 22:51:22 -0300

    Criação do projeto ContaBanco para desafio dio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Por favor digite o numero da conta: ");
+		int numero = sc.nextInt();
+
+		System.out.print("Por favor digite o numero da Agência: ");
+		String agencia = sc.next();
+		sc.nextLine();
+
+		System.out.print("Por favor digite o nome do cliente: ");
+		String nomeCliente = sc.nextLine();
+
+		System.out.print("Por favor digite o saldo: ");
+		double saldo = sc.nextDouble();
+
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+				+ agencia + ", conta " + numero + " e seu saldo " + saldo + " ja está disponivel para saque.");
+
+		sc.close();
+
+	}
+
+}
