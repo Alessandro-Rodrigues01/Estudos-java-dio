0479699b20055dc1b0e77fc42d84515d796d0b81
Alessandro <alessandro.rodrigues089@gmail.com>
2024-06-24 20:29:27 -0300

    Novos projetos criados 24-06-2024

---
+++ collections/src/application/Tarefa.java
@@ -0,0 +1,27 @@
+package application;
+
+public class Tarefa {
+	private String descricao;
+
+	public Tarefa(String descricao) {
+		super();
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+
+	
+
+
+
+	
+	
+
+}
